94e12f77c0fa1fea698b4e8473b6e18772c4ddcf
UWANYAGASANI Jerome <uwanyagasanijerome@gmail.com>
2025-04-06 23:11:12 +0200

    Update: <describe your change>

---
+++ wsgi.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr  6 21:27:25 2025
+
+@author: uwany
+"""
+
+from app import app  # Replace 'your_application' with your actual app module name
+
+if __name__ == "__main__":
+    app.run()
